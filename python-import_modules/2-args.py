c6426968b48f2202398cfbbc020b20c9d87b4b88
thomas rousseau <thomas.rousseau19@gmail.com>
2025-05-09 10:55:54 +0200

    r

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 import sys
-if len(sys.argv) - 1 == 1:
-    print("{} argument:".format(len(sys.argv)-1))
-if len(sys.argv) - 1 == 0:
-    print("0 arguments.")
-else:
-    print("{} arguments:".format(len(sys.argv)-1))
-for i in sys.argv[1:]:
-    print(i)
+if __name__ == "__main__":
+    len_args = len(sys.argv) - 1 
+    if len_args == 0:
+        print("0 arguments.")
+    elif len_args == 1:
+        print("1 argument :")
+    else:
+        print(f"{len_args} arguments:")
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i,sys.argv[i]))
