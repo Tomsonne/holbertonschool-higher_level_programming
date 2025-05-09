c38e968a7920cf32290548bb4f4b5e79124fe93a
thomas rousseau <thomas.rousseau19@gmail.com>
2025-05-09 10:49:34 +0200

    r

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 import sys
+if len(sys.argv) - 1 == 1:
+    print("{} argument :".format(len(sys.argv)-1))
 if len(sys.argv) - 1 == 0:
-    print("{} argument".format(len(sys.argv)-1))
+    print("0 arguments.")
 else:
-    print("{} arguments".format(len(sys.argv)-1))
+    print("{} arguments :".format(len(sys.argv)-1))
 for i in sys.argv[1:]:
     print(i)
