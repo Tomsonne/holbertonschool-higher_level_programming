e977d2fe7388bd760cdd1bbb6ec7dd952dffa0f3
Tomsonne <thomas.rousseau19@gmail.com>
2025-05-09 13:45:27 +0200

    Update 4-hidden_discovery.py

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import importlib.util
+    import hidden_4
 
-    spec = importlib.util.spec_from_file_location("hidden_4",
-                                                  "/tmp/hidden_4.pyc")
-    hidden_4 = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(hidden_4)
-    for name in sorted(dir(hidden_4)):
+    names = dir(hidden_4)
+    for name in sorted(names):
         if not name.startswith("__"):
             print(name)
