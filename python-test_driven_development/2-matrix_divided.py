afc3152adbc1c4150f65f5babfaba08e1ae267b1
thomas rousseau <thomas.rousseau19@gmail.com>
2025-05-17 17:17:46 +0200

    r

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def matrix_divided(matrix, div):
     list_error = "matrix must be a matrix (list of lists) of integers/floats"
     len_error = "Each row of the matrix must have the same size"
