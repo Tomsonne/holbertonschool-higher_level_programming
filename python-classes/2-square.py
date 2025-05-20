3881203bb3553281c1fd3eaadc8da1ebc7270ddc
thomas rousseau <thomas.rousseau19@gmail.com>
2025-05-20 15:47:30 +0200

    r

---
+++ python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Définition d'une classe vide Square (carré)"""
+
+
+class Square:
+    """Classe qui définit un carré avec un attribut privé : la taille"""
+
+    def __init__(self, size=0):
+        """Initialisation d'une instance de Square
+
+        Args:
+            size: taille du carré (doit être un entier >= 0)
+
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size  # Attribut privé
