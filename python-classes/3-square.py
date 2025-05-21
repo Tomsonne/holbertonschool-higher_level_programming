79f77fc9fda7dc122b3aa4d759f5e740af120bc2
thomas rousseau <thomas.rousseau19@gmail.com>
2025-05-21 11:27:48 +0200

    r

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -17,7 +17,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
 
-        self.__size = size  # Attribut privé
+        self.__size = size
 
     def area(self):
         """Calcule et retourne l'aire du carré"""
