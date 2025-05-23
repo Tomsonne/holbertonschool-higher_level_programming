488130ec6dcca414a659efb3bbd890aa79a2bda0
thomas rousseau <thomas.rousseau19@gmail.com>
2025-05-23 09:44:31 +0200

    r

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""Définition d'une classe vide Square (carré)"""
+"""Définition d'une classe vide Rectangle """
 
 
 class rectangle:
     """Classe qui ne fait rien pour l'instant (classe vide)"""
+    
     pass
