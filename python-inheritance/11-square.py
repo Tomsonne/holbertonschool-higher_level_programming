e21dc26bd4c8aa4ce7efe58dbfd11f6d2f08eb6b
thomas rousseau <thomas.rousseau19@gmail.com>
2025-05-28 00:06:33 +0200

    r

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -12,9 +12,9 @@ class Square(Rectangle):
     def __init__(self, size):
         """Methode"""
 
-        super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
         """rectangle area"""
