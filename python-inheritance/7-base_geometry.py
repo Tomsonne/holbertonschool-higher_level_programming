6cdbdb76f23ce6afd96d79dffa950144d7928c67
thomas rousseau <thomas.rousseau19@gmail.com>
2025-05-28 00:16:08 +0200

    r

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,21 +1,33 @@
 #!/usr/bin/python3
 """
-bip
-bip
+Module 7-base_geometry
+Ce module définit une classe de base pour la géométrie.
 """
 
 
 class BaseGeometry:
-    """BaseGeometry class"""
+    """Classe de base pour les formes géométriques."""
 
     def area(self):
-        """calcul area"""
+        """
+        Calcule l'aire de la forme géométrique.
+        Doit être surchargée par les classes filles.
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """valid int ?"""
+        """
+        Valide que `value` est un entier positif.
 
+        Args:
+            name (str): Le nom du paramètre.
+            value (int): La valeur à valider.
+
+        Raises:
+            TypeError: Si `value` n'est pas un entier.
+            ValueError: Si `value` est inférieur ou égal à 0.
+        """
         if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError(f"{name} must be greater than 0")
