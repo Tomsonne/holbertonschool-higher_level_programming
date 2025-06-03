f898d31e3090817fb3c6733f20079e8361b3426f
thomas rousseau <thomas.rousseau19@gmail.com>
2025-06-03 15:59:20 +0200

    r

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
+"""Reads"""
+
+
 def read_file(filename=""):
     """Reads"""
     with open(filename, 'r', encoding='utf-8') as f:
--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -10,4 +10,3 @@ def append_write(filename="", text=""):
     """
     with open(filename, "a") as f:
         return f.write(text)
-    
