1be1b52972d5dfba9d63acdef760c8ad23d8d1a8
thomas rousseau <thomas.rousseau19@gmail.com>
2025-06-03 15:54:38 +0200

    r

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""READ"""
+
+
+def to_json_string(my_obj):
+    """Convert"""
+    import json
+
+    return json.dumps(my_obj)
---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""READ"""
+
+
+def from_json_string(my_str):
+    """Convert"""
+    import json
+
+    return json.loads(my_str)
