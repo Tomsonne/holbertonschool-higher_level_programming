fe19c93799c3361c6b386d44803361a207029b82
thomas rousseau <thomas.rousseau19@gmail.com>
2025-06-04 11:34:01 +0200

    r

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -2,26 +2,20 @@
 """My class module"""
 
 
-def pascal_triangle(n):    
+def pascal_triangle(n):
     """
     Retourne
     """
-
-
     if n <= 0:
         return []
-    
-    triangle = [[1]] 
-    
+    triangle = [[1]]
     for ligne in range(1, n):
         nouvelle_ligne = [1]
         ligne_precedente = triangle[ligne - 1]
-
         for colonne in range(1, ligne):
             valeur = ligne_precedente[colonne - 1] + ligne_precedente[colonne]
             nouvelle_ligne.append(valeur)
-        
         nouvelle_ligne.append(1)
         triangle.append(nouvelle_ligne)
-    
+
     return triangle
