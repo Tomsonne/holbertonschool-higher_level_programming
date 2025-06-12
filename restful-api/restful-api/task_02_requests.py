47ec0bfb91194001bea2d2e31d804a2b99e8dc98
thomas rousseau <thomas.rousseau19@gmail.com>
2025-06-12 11:07:34 +0200

    r

---
+++ restful-api/restful-api/task_02_requests.py
@@ -0,0 +1,35 @@
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    print(f"Status Code: {response.status_code}")
+
+    if response.status_code == 200:
+        posts = response.json()  # conversion JSON -> liste de dictionnaires
+        for post in posts:
+            print(post['title'])  # on affiche le titre de chaque post
+    else:
+        print("Échec de la récupération des données.")
+
+def fetch_and_save_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        posts = response.json()
+        filtered_posts = [
+            {"id": post["id"], "title": post["title"], "body": post["body"]}
+            for post in posts
+        ]
+
+        with open("posts.csv", mode="w", newline='', encoding="utf-8") as csvfile:
+            fieldnames = ["id", "title", "body"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            writer.writerows(filtered_posts)
+    else:
+        print("Impossible de sauvegarder les données, requête échouée.")
