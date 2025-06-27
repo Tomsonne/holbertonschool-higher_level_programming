b93c4f3af7bae70a5eff7c6730f38556f319bbab
thomas rousseau <thomas.rousseau19@gmail.com>
2025-06-27 18:27:50 +0200

    r

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    """
+    popo
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db_url = f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}"
+
+    engine = create_engine(db_url, pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name == state_name).first()
+    if state:
+        print(f"{state.id}")
+    else:
+        print("Not found")
