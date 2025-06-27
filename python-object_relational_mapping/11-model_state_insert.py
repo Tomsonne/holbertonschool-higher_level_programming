578cc86e1e9c5c5ccf5e2dd69db7dcee6f63c5f0
thomas rousseau <thomas.rousseau19@gmail.com>
2025-06-27 19:39:20 +0200

    r

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-"""Adds the State object 'Louisiana' to the database."""
+"""
+popo
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
+    """
+    popo
+    """
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
