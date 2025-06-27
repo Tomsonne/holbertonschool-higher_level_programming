35a36ad7ae6a81763b39370ea8583d1de0f231de
thomas rousseau <thomas.rousseau19@gmail.com>
2025-06-27 14:46:50 +0200

    r

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Définition de la classe State liée à la table 'states' dans la base MySQL."""
+"""Définition """
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-# Base est le constructeur de classe-mère pour tous les modèles
 Base = declarative_base()
+
 
 class State(Base):
     """Classe qui mappe la table 'states' dans la base MySQL"""
