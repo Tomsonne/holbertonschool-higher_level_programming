bbce393c49712bfe7d7f2b4b5caed6fa010c06a6
thomas rousseau <thomas.rousseau19@gmail.com>
2025-06-27 18:29:48 +0200

    r

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""
+popo
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+
 
 if __name__ == "__main__":
     """
