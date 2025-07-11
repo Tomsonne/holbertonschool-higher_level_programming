85ecba2e7d17778cd5858a569053c782073edc18
thomas rousseau <thomas.rousseau19@gmail.com>
2025-07-11 16:47:47 +0200

    r

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const x = parseInt(process.argv[2]);
+const size = parseInt(process.argv[2]);
 
-if (!isNaN(x) && x > 0) {
-  for (let i = 0; i < x; i++) {
-    console.log('C is fun');
+if (!isNaN(size) && size > 0) {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
   }
 } else {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 }
