737c1e7da222fa12eb6ce26ac567e363e66f97a7
thomas rousseau <thomas.rousseau19@gmail.com>
2025-07-11 11:25:57 +0200

    r

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 
 const argCount = process.argv.slice(2);
 
-if (argCount === 0) {
+if (argCount.lenght === 0) {
     console.log('No argument');
-} else if (argCount === 1) {
+} else if (argCount.lenght === 1) {
     console.log('Argument found');
 } else {
     console.log('Arguments found');
