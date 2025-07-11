cf2045789ee8c8653585c83e1b6abe4c5cd69a43
thomas rousseau <thomas.rousseau19@gmail.com>
2025-07-11 16:46:29 +0200

    r

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -4,9 +4,9 @@ const firstArg = process.argv[2];
 const secondArg = process.argv[3];
 
 if (firstArg !== undefined && secondArg !== undefined) {
-    console.log(firstArg + ' is ' + secondArg);
+  console.log(firstArg + ' is ' + secondArg);
 } else if (firstArg !== undefined) {
-    console.log(firstArg + ' is undefined');
+  console.log(firstArg + ' is undefined');
 } else {
-    console.log('undefined is undefined');
+  console.log('undefined is undefined');
 }
