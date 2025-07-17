c749303c77cb37be189ae22877ed2aa621d766e8
thomas rousseau <thomas.rousseau19@gmail.com>
2025-07-17 12:06:58 +0200

    r

--- javascript-dom_manipulation/8-script.js
+++ javascript-dom_manipulation/8-script.js
@@ -1,9 +1,7 @@
-fetch("https://hellosalut.stefanbohacek.dev/?lang=fr")
-  .then(response => response.json())
-  .then(data => {
-    const hello = document.getElementById('hello');
-    hello.textContent = data.hello;
-  })
-  .catch(error => {
-    console.error("Erreur :", error);
-  });
+document.addEventListener('DOMContentLoaded', () => {
+  fetch("https://hellosalut.stefanbohacek.dev/?lang=fr")
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById('hello').textContent = data.hello;
+    });
+});
