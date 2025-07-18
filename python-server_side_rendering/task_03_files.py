39b78dfa86e5c4fed01b072caa62dc73239a75d8
thomas rousseau <thomas.rousseau19@gmail.com>
2025-07-18 20:36:15 +0200

    r

--- python-server_side_rendering/task_03_files.py
+++ python-server_side_rendering/task_03_files.py
@@ -1,19 +1,55 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
+import csv
 
 app = Flask(__name__)
 
-@app.route('/items')
-def items():
+def read_json():
     try:
-        with open('items.json', 'r') as file:
-            data = json.load(file)
-            items_list = data.get("items", [])
-    except (FileNotFoundError, json.JSONDecodeError) as e:
-        print("Erreur :", e)
-        items_list = []
+        with open("products.json", "r") as file:
+            return json.load(file)
+    except Exception:
+        return []
 
-    return render_template('items.html', items=items_list)
+def read_csv():
+    products = []
+    try:
+        with open("products.csv", newline='') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                products.append({
+                    "id": int(row["id"]),
+                    "name": row["name"],
+                    "category": row["category"],
+                    "price": float(row["price"])
+                })
+    except Exception:
+        pass
+    return products
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+@app.route("/products")
+def display_products():
+    source = request.args.get("source")
+    id_str = request.args.get("id")
+
+    if source == "json":
+        data = read_json()
+    elif source == "csv":
+        data = read_csv()
+    else:
+        return render_template("product_display.html", error="Wrong source")
+
+    if id_str:
+        try:
+            id_int = int(id_str)
+            filtered = [item for item in data if item["id"] == id_int]
+            if not filtered:
+                return render_template("product_display.html", error="Product not found")
+            return render_template("product_display.html", products=filtered)
+        except ValueError:
+            return render_template("product_display.html", error="Invalid ID format")
+
+    return render_template("product_display.html", products=data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
